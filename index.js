07c9ca14eed3ead108890995899bd500f8835d60
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-04-30 16:30:08 +0200

    Several minor changes has been done because of facng problem with sever link placement

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require("dotenv").config();
 
 
 //middleware 
@@ -61,12 +61,12 @@ app.get('/myLocation/', async (req, res) => {
     try {
         const result = await assignment10Data.find({}).toArray();
         res.json(result);
+        // res.send(result);
     } catch (error) {
         console.error("Error retrieving data:", error);
         res.status(500).json({ error: "Failed to retrieve data" });
     }
 });
-
 
 
 //Read the posted data in server at MyListPage
@@ -77,18 +77,36 @@ app.get('/myLocation/:email', async (req, res) => {
     res.send(result)
 })
 
+// app.get('/myLocation/:id', async (req, res) => {
+//     try {
+//         const id = req.params.id;
+//         const query = { _id: new ObjectId(id) };
+//         const result = await assignment10Data.find(query).toArray();
+//         res.json(result);
+//     } catch (error) {
+//         console.error("Error retrieving data:", error);
+//         // Send a single error response with status code
+//         res.status(500).json({ error: "Failed to retrieve data" });
+//     }
+// });
+
+
 //country name specific data for italy 
 app.get('/myLocation/', async (req, res) => {
     try {
         const result = await assignment10Data.find({ countryName: "Italy" }).toArray();
         console.log(result);
         res.json(result);
+        // res.send(result);
         
     } catch (error) {
         console.error("Error retrieving data:", error);
         res.status(500).json({ error: "Failed to retrieve data" });
     }
 });
+
+
+
 
 
 
@@ -102,30 +120,37 @@ app.delete('/myLocation/:id', async (req, res) => {
 })
 
 // update 
+
 app.put('/myLocation/:id', async (req, res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) };
-    const options = { upsert: true };
-    const updatedLocation = req.body;
-    const locationUpdate = {
-        $set: {
-            touristSpotName: updatedLocation.touristSpotName,
-            countryName: updatedLocation.countryName,
-            locationName: updatedLocation.locationName,
-            numberOfVisitors: updatedLocation.numberOfVisitors,
-            cost: updatedLocation.cost,
-            image: updatedLocation.image,
-            season: updatedLocation.season,
-            travelDuaration: updatedLocation.travelDuaration,
-            description: updatedLocation.description,
-            userName: updatedLocation.userName
-        }
-    };
+    try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatedLocation = req.body;
+        const locationUpdate = {
+            $set: {
+                touristSpotName: updatedLocation.touristSpotName,
+                countryName: updatedLocation.countryName,
+                locationName: updatedLocation.locationName,
+                numberOfVisitors: updatedLocation.numberOfVisitors,
+                cost: updatedLocation.cost,
+                image: updatedLocation.image,
+                season: updatedLocation.season,
+                travelDuaration: updatedLocation.travelDuaration,
+                description: updatedLocation.description,
+                userName: updatedLocation.userName
+            }
+        };
 
-    const result = await assignment10Data.updateOne(filter, locationUpdate, options);
-    res.send(result);
-
-})
+        const result = await assignment10Data.updateOne(filter, locationUpdate, options);
+        // Send response once the update operation is completed
+        res.json(result);
+    } catch (error) {
+        console.error("Error updating location:", error);
+        // Handle error response if needed
+        res.status(500).json({ error: "Failed to update location" });
+    }
+});
 
 
 //Basic server function for web query
